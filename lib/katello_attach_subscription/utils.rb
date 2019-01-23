7e63d2315fcb3f262a679cd2c8988a307d4eb9cb
Simone Mossi <mossicrue@gmail.com>
2019-01-23 10:38:46 +0100

    Change from clone to dub

--- lib/katello_attach_subscription/utils.rb
+++ lib/katello_attach_subscription/utils.rb
@@ -16,23 +16,17 @@ module KatelloAttachSubscription
     end
 
     def self.merge_subs(current_sub, sub_to_merge, command)
-      if not current_sub.nil?
-        current = current_sub.clone
-      else
-        current = {}
-      end
-      new_sub = sub_to_merge.clone
       case command
       when "override"
-        current = new_sub
+        merged_sub = sub_to_merge.dup
       else
-        if current.nil?
-          current = new_sub
+        if current_sub.nil?
+          merged_sub = sub_to_merge.dup
         else
-          current.merge!(new_sub)
+          merged_sub = current_sub.merge(sub_to_merge)
         end
       end
-      return current.clone
+      return merged_sub
     end
   end
 end
