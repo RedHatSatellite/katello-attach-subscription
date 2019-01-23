8b5fc9ef1a97e423ef7407eefd58964a3cde3101
Simone Mossi <mossicrue@gmail.com>
2019-01-23 14:38:07 +0100

    Possible layering for subscription rules (#43)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,29 @@
 require 'json'
+require 'yaml'
 require 'katello_attach_subscription'
 require 'minitest/autorun'
 
 def read_json_fixture(file)
   json = File.expand_path(File.join('..', 'fixtures', "#{file}.json"), __FILE__)
   JSON.parse(File.read(json))
+end
+
+def read_yaml_fixture(file)
+  yaml = File.expand_path(File.join('..', 'fixtures', "#{file}.yaml"), __FILE__)
+  YAML.load_file(yaml)
+end
+
+def get_sub_for_host(subs, host)
+  parsed_final = nil
+  subs.each do |single_sub|
+    parsed = single_sub['sub_parsed']
+    if KatelloAttachSubscription::HostMatcher.match_host(host, single_sub)
+      layer_command = single_sub['sub_layer'] || 'stop_parsing'
+      parsed_final = KatelloAttachSubscription::Utils.merge_subs(parsed_final, parsed, layer_command)
+      if layer_command == 'stop_parsing'
+        break
+      end
+    end
+  end
+  return parsed_final
 end
